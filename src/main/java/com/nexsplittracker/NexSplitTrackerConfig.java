6602e1eb88e8476c92439cd68ce69560339f510e
Rocket <118800540+KeyboardIsMagic@users.noreply.github.com>
2024-01-11 16:47:33 -0800

    Removed unused code due to refactoring how items are handled

--- src/main/java/com/nexsplittracker/NexSplitTrackerConfig.java
+++ src/main/java/com/nexsplittracker/NexSplitTrackerConfig.java
@@ -1,6 +1,5 @@
 package com.nexsplittracker;
 
-import net.runelite.client.config.Alpha;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
